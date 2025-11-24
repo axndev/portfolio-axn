f999180b89e576f2d438c92795225056c2e4f1d6
Kaleemullah <kaleemullahahsan0@gmail.com>
2025-11-24 19:13:21 +0500

    Projects

--- src/data/projects.js
+++ src/data/projects.js
@@ -13,13 +13,14 @@ export const projectsData  = [
         "desc": "Development version of Jona Panels — a company dealing in high-quality surface protectors and adhesives.",
         "url": "https://jonapanels.kinsta.cloud/"
     },
-    {
-        "icon": "centerstagehomestaging.jpg",
+        {
+        "icon":"easycalculator.png",
         "id": 3,
-        "title": "Center Stage Home Staging (Staging)",
-        "desc": "Staging environment for a home-staging business that enhances property appeal for sale.",
-        "url": "https://centerstagehomestaging.kinsta.cloud/"
+        "title": "Easy Calculator",
+        "desc" :"A web app offering a variety of calculators and converters (math operations, unit conversions, etc.).",
+        "url":"https://easycalculator.tech"
     },
+   
     {
         "icon": "kelownaphysicianssociety.png",
         "id": 4,
@@ -34,54 +35,55 @@ export const projectsData  = [
         "desc": "Corporate staging site for Thomas Pride International, likely offering international business or trade services.",
         "url": "https://thomasprideinternational.kinsta.cloud/"
     },
-    {
-        "icon": "mindrelicconsulting.jpg",
-        "id": 6,
-        "title": "Mind Relic Consulting",
-        "desc": "An Australian consulting firm offering contractor management, financial advisory, and business transformation services.",
-        "url": "https://mindrelicconsulting.com.au/"
-    },
-    {
-        "icon":"easycalculator.png",
-        "id": 7,
-        "title": "Easy Calculator",
-        "desc" :"A web app offering a variety of calculators and converters (math operations, unit conversions, etc.).",
-        "url":"https://easycalculator.tech"
-    },
+
     {
         "icon":"awaisco.jpg",
-        "id": 8,
+        "id": 6,
         "title": "Awaisco",
         "desc" :"A professional accounting & finance services site (accounts, taxation, audit).",
         "url":"http://awaisco.com"
     },
     {
         "icon":"pehnawa-clothing.jpg",
-        "id": 9,
+        "id": 7,
         "title": "Pehnawa Clothing",
         "desc" :"E-commerce site for women’s clothing, featuring catalog, product pages, online ordering.",
         "url":"https://pehnawaclothing.com"
     },
     {
         "icon":"golfriverside.png",
-        "id": 10,
+        "id": 8,
         "title": "Golf Riverside",
         "desc" :"Real estate / property development site for riverfront & golf course lots.",
         "url":"https://golfriverside.com"
     },
     {
         "icon":"ccsocd.png",
-        "id": 11,
+        "id": 9,
         "title": "Canadian Carmelite Charitable Society",
         "desc" :"Nonprofit / retreat center website that shares information about spiritual retreats, projects, donation, congregational activities.",
         "url":"https://cccsocd.ca"
     },
     {
         "icon":"chatgpt.png",
-        "id": 12,
+        "id": 10,
         "title": "ChatGPT Clone",
         "desc" :"A ChatGPT Clone web app built using the Gemini API, Vue.js, and Tailwind CSS.",
         "url":"https://axn-chatgpt.netlify.app"
+    },
+        {
+        "icon": "mindrelicconsulting.jpg",
+        "id": 11,
+        "title": "Mind Relic Consulting",
+        "desc": "An Australian consulting firm offering contractor management, financial advisory, and business transformation services.",
+        "url": "https://mindrelicconsulting.com.au/"
+    },
+     {
+        "icon": "centerstagehomestaging.jpg",
+        "id": 12,
+        "title": "Center Stage Home Staging (Staging)",
+        "desc": "Staging environment for a home-staging business that enhances property appeal for sale.",
+        "url": "https://centerstagehomestaging.kinsta.cloud/"
     },
     {
         "icon":"favicon.png",
